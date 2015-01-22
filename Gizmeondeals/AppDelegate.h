672379bff07a28e54a275d7c1efa1a3ac124b246
maazzaam <royleelaelectronics@Roys-MacBook-Pro.local>
2015-01-22 10:07:10 +0530

    Initial Commit

---
+++ Gizmeondeals/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Gizmeondeals
+//
+//  Created by Roy Leela Electronics on 22/01/15.
+//  Copyright (c) 2015 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
